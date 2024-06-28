ae504f8e7c4e3ad7cd2c88b0b8f492b7a5b442c8
MatheusMenezesss <159057394+MatheusMenezesss@users.noreply.github.com>
2024-06-28 14:45:21 -0300

    Update timer.c

--- timer.c
+++ timer.c
@@ -92,8 +92,12 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  if(timer_elapsed(start) < ticks){
+    thread_sleep(start + ticks);// implementar a função
+
+  }
+  //while (timer_elapsed (start) < ticks) 
+    //thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
